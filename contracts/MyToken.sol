919612249f5c677267bf41e02b658eafda671756
alfamike <amenachorodriguez@al.uloyola.es>
2024-05-12 20:06:10 +0200

    Initial commit: remix template ozerc20

---
+++ contracts/MyToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract MyToken is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("MyToken", "MTK");
+        __ERC20Permit_init("MyToken");
+    }
+}
