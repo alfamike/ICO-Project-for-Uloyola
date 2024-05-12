00f809e8122a9ea0f3a6fc4ae87c858a859c18db
alfamike <amenachorodriguez@al.uloyola.es>
2024-05-12 20:08:50 +0200

    Initial contract commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,11 +8,13 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 contract MyToken is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
+        initialize();
         _disableInitializers();
     }
 
     function initialize() initializer public {
         __ERC20_init("MyToken", "MTK");
         __ERC20Permit_init("MyToken");
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
